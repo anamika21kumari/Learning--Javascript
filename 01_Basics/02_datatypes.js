2d2de9282d6eda6f0e1eb991df1054e3caef8bc9
Anamika Kumari <anabellaanu@gmail.com>
2026-02-28 05:50:42 +0000

    dataTypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"//treates JS as newer version 
+
+let name="Anamika"
+let age=18;
+let isAdult=true;
+
+//Primitive datatypes
+//symbol=>unique
+//bigint=.stores large range numbers
+//number=2 to power 53
+//boolean=>true/false
+//null
+//undefined
+
+//object
+
+
+console.table([age,name,isAdult])
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isAdult)
+
+
+
+
+console.log("type of null=> ",typeof null)
+console.log("type of undefined=> ",typeof undefined)
